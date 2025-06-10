1eb374a4fa864d7544a60c5a5abb6ee8fd8a3f30
BrandonDL603 <brandonlambrinos@gmail.com>
2025-06-09 20:39:16 -0400

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,6 +6,10 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    externalSaml: {
+      providerName: 'Entra',
+      metadataUrl: 'https://login.microsoftonline.com/56a51620-6c5b-4c7b-a71b-845bbe83069a/federationmetadata.xml',
+      callbackUrls: ['https://dev.d2uluvkyv859go.amplifyapp.com/'],
+      logoutUrls: ['https://dev.d2uluvkyv859go.amplifyapp.com/'],
   },
 });
